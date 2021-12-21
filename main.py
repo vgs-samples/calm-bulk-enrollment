9e8dd38eb5ebcb850714fff41ee2703055a2d88d
Yuriy Yunikov <yuriy.yunikov@vgs.io>
2021-12-21 11:06:02 +0200

    Added error codes example (#5)
    * Addded example of cards processing
    
    * Fixes

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 import csv
 import json
 import os
@@ -62,12 +64,17 @@ def alias_raw_data():
             aliased.writerow(header)
             for card in cards:
                 card_number = card[1]
-                print(f'Aliasing card number {card_number[6:]}******{card_number[:4]}')
-                r = requests.post(f'https://{vault_id}.{environment}.verygoodproxy.com/post',
-                                  json={'account_number': card_number}
-                                  )
-                alias = json.loads(r.json()['data'])['account_number']
-                aliased.writerow([card[0], alias, card[2], card[3]])
+                hidden_card_number = f'{card_number[:6]}******{card_number[-4:]}'
+                print(f'Aliasing card number {hidden_card_number}')
+                try:
+                    r = requests.post(f'https://{vault_id}.{environment}.verygoodproxy.com/post',
+                                      json={'account_number': card_number}
+                                      )
+                    alias = json.loads(r.json()['data'])['account_number']
+                    aliased.writerow([card[0], alias, card[2], card[3]])
+                    print(f'Card {hidden_card_number} was successfully aliased')
+                except requests.exceptions.RequestException as e:
+                    print("Failed to alias card: ", e)
     print(f'Successfully aliased raw data in vault {vault_id}. See "csv/aliased_cards.csv" for created aliases.')
 
 
@@ -84,28 +91,63 @@ def enroll_data(aliased=False):
             updated.writerow(header)
             for card in cards:
                 card_number = card[1]
-                print(f'Enrolling card number {card_number[6:]}******{card_number[:4]}')
+                hidden_card_number = f'{card_number[:6]}******{card_number[-4:]}'
+                print(f'Enrolling card number {hidden_card_number}')
                 proxies = {
                     'https': f'https://{vault_username}:{vault_password}@{vault_id}.{environment}.verygoodproxy.com:8080'}
-                r = requests.post(f'https://calm.{environment}.verygoodsecurity.app/cards',
-                                  json=request_data(card[0], card_number, card[2], card[3]),
-                                  headers={
-                                      'Content-Type': 'application/json',
-                                      'Authorization': f'Bearer {fetch_jwt()}'
-                                  },
-                                  proxies=proxies if aliased else None,
-                                  verify=Path(__file__).resolve().parent / f'certs/{environment}_cert.pem' if aliased else None
-                                  )
-                data = r.json()['data']
-                card_id = data['id']
-                name = data['name']
-                updated_number = data['number']
-                updated_exp_month = data['exp_month']
-                updated_exp_year = data['exp_year']
-                updated.writerow([card_id, name, updated_number, updated_exp_month, updated_exp_year])
+                try:
+                    r = requests.post(f'https://calm.{environment}.verygoodsecurity.app/cards',
+                                      json=request_data(card[0], card_number, card[2], card[3]),
+                                      headers={
+                                          'Content-Type': 'application/json',
+                                          'Authorization': f'Bearer {fetch_jwt()}'
+                                      },
+                                      proxies=proxies if aliased else None,
+                                      verify=Path(__file__).resolve().parent / f'certs/{environment}_cert.pem' if aliased else None
+                                      )
+                    r.raise_for_status()
+                    write_response_to_file(r, updated)
+                    print(f'Card {hidden_card_number} was successfully enrolled')
+                except requests.exceptions.HTTPError as err:
+                    print(f'Failed to enroll card {card_number}. Errors:',
+                          [error_msg for error_msg in get_error_messages(err)])
+                except requests.exceptions.RequestException as e:
+                    print(f'Failed to enroll card. Status code : {e.response.status_code}, Message: {err.response.text}')
     print(
         f'Successfully finished bulk enrollment process. '
         f'See "csv/updated_cards.csv" for enrolled card with their corresponding ID\'s.')
+
+
+def get_error_messages(err: requests.exceptions.HTTPError) -> str:
+    if err.response.text and json.loads(err.response.text)['errors']:
+        for error in json.loads(err.response.text)['errors']:
+            details = error['detail']
+            code = error['code']
+            if code == 'merchant-not-found':
+                print("No merchant was registered for organization. Please see https://www.verygoodsecurity.com/docs/payment-optimization/calm/account-updater/onboarding")
+                print("Stopping cards processing.")
+                sys.exit(1)
+            yield {
+                # see https://www.verygoodsecurity.com/docs/payment-optimization/calm/api/api-reference for list of error codes
+                'card-brand-not-supported': "Provided card brand is not supported. Please see https://www.verygoodsecurity.com/docs/payment-optimization/calm/account-updater",
+                'validation-failed': f'Validation error: {details}',
+                'internal-server-error': f'Internal server error: {details}',
+                # error codes below should never happen
+                'invalid-payload': f'Invalid payload: {details}',
+                'unsupported-media-type': f'Media type is not supported: {details}'
+            }[code]
+    else:
+        return f'Failed to enroll card. Status code {err.response.status_code}:, Message: {err.response.text}'
+
+
+def write_response_to_file(response, file):
+    data = response.json()['data']
+    card_id = data['id']
+    name = data['name']
+    updated_number = data['number']
+    updated_exp_month = data['exp_month']
+    updated_exp_year = data['exp_year']
+    file.writerow([card_id, name, updated_number, updated_exp_month, updated_exp_year])
 
 
 if __name__ == "__main__":
